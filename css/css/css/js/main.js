41f84c4335a1e9139121e471e86605ec6e5b322c
divyarajsingh2021-pixel <divyarajsingh2021@gmail.com>
2026-07-26 22:06:04 +0530

    Update main.js

--- css/css/css/js/main.js
+++ css/css/css/js/main.js
@@ -1,1 +1,33 @@
+let progress = 0;
 
+const bar = document.getElementById("loadingProgress");
+
+const loading = document.getElementById("loadingScreen");
+
+const menu = document.getElementById("mainMenu");
+
+const play = document.getElementById("playBtn");
+
+const timer = setInterval(()=>{
+
+progress+=2;
+
+bar.style.width=progress+"%";
+
+if(progress>=100){
+
+clearInterval(timer);
+
+loading.style.display="none";
+
+}
+
+},30);
+
+play.onclick=()=>{
+
+menu.style.display="none";
+
+alert("Welcome to DSC Alpha v0.1");
+
+};
